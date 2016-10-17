b897030f8457525f002ecd992fdc3415742a0231
rshapass <rshapass@gmail.com>
2016-10-17 18:51:16 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var bubbles = [];
+
+function setup() {
+  createCanvas(600,400);
+  bubbles[0]= new Bubble (250,200);
+  bubbles [1]= new Bubble (350,200);
+}
+
+function draw() {
+  background(0);
+  bubbles[0].update();
+  bubbles[1].update();
+  bubbles[0].display();
+  bubbles[1].display();
+  if (bubbles[0].intersects(bubbles[1])){
+    bubbles[0].changeColor();
+    bubbles[1].changeColor();
+  }
+}
+  
